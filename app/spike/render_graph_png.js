6a351d43360458877418bdf93ddc0d27900de2a3
Paulo Schneider <paulo.schneider@gmail.com>
2013-10-11 16:29:15 +0100

    Render a graph from performance platform

--- app/spike/render_graph_png.js
+++ app/spike/render_graph_png.js
@@ -8,7 +8,9 @@ function (baseTemplate) {
     phantom.create(function (ph) {
       return ph.createPage(function(page) {
         return page.open('http://localhost:3000/view/graph', function (status) {
+            page.zoomFactor = 0.25;
             page.render('graph.png');
+            ph.exit();
             res.sendfile('graph.png');
         });
       });
