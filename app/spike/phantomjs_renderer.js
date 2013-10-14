bf9c0f312788cb5d91570d8c4dea22fb0923ac2d
Paulo Schneider <paulo.schneider@gmail.com>
2013-10-14 15:16:22 +0100

    Create a single instance of phantomjs, for the queue

--- app/spike/phantomjs_renderer.js
+++ app/spike/phantomjs_renderer.js
@@ -1,21 +1,24 @@
 define(
 function () {
+  var phantom = require('phantom');
+  var fs = require('fs');
+  var phantomInstance;
+  
+  phantom.create(function (ph) {
+    phantomInstance = ph;
+  });
+
 
   return function (task, callback) {
     console.log("New task received");
-    var phantom = require('phantom');
-    var fs = require('fs');
 
-    phantom.create(function (ph) {
-      return ph.createPage(function(page) {
-        page.set('viewportSize', { width: 1000, height: 1000 });
-        return page.open(task.url, function (status) {
-            page.render('./graph.png', function () {
-              console.log("Task processed")
-              ph.exit();
-              callback(null, './graph.png');
-            });
-        });
+    return phantomInstance.createPage(function(page) {
+      page.set('viewportSize', { width: 1000, height: 1000 });
+      return page.open(task.url, function (status) {
+          page.render('./graph.png', function () {
+            callback('./graph.png');
+            console.log("Task processed")
+          });
       });
     });
   }
--- app/spike/render_graph_png.js
+++ app/spike/render_graph_png.js
@@ -5,7 +5,7 @@ function (baseTemplate) {
 
   return function (queue) {
     return function render (req, res) {
-      queue.push({url: 'http://localhost:3000/view/graph'}, function (err, file) {
+      queue.push({url: 'http://localhost:3000/view/graph'}, function (file) {
         res.sendfile(file);
       });
     }
