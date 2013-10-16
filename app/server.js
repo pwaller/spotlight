eaa4f210e9f9cbc75a4e8809d695e98216a840dd
Jacob Ashdown <jcbashdown@googlemail.com>
2013-10-16 16:14:58 +0100

    Stop trying this for now - in require hell

--- app/server.js
+++ app/server.js
@@ -1,71 +1,68 @@
-var requirejs = require('requirejs');
-var config = requirejs('./config');
-config.baseUrl = 'app/';
-config.nodeRequire = require;
-requirejs.config(config);
+define(['requirejs', 'optimist', 'express', 'http', 'path', 'jquery', 'xmlhttprequest'], function (requirejs, optimist, express, http, path, jquery, xmlhttprequest) {
+  var config = requirejs('./config');
+  config.baseUrl = 'app/';
+  config.nodeRequire = require;
+  requirejs.config(config);
 
-var argv = require('optimist').argv;
+  var argv = optimist.argv;
 
-var express = require('express'),
-    http = require('http'),
-    path = require('path');
-
-
-global.isServer = true;
-global.requirePath = argv.REQUIRE_BASE_URL || '/app/';
-global.assetPath = '/assets/';
+  global.isServer = true;
+  global.requirePath = argv.REQUIRE_BASE_URL || '/app/';
+  global.assetPath = '/assets/';
 
 
-var $ = global.$ = global.jQuery = require('jquery');
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-$.support.cors = true;
-$.ajaxSettings.xhr = function () {
-    return new XMLHttpRequest();
-};
+  var $ = global.$ = global.jQuery = jquery;
+  var XMLHttpRequest = xmlhttprequest.XMLHttpRequest;
+  $.support.cors = true;
+  $.ajaxSettings.xhr = function () {
+      return new XMLHttpRequest();
+  };
 
-var rootDir = path.join(__dirname, '..');
+  var rootDir = path.join(__dirname, '..');
 
-var app = express();
+  var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.compress());
-  app.use('/assets', express.static(path.join(rootDir, 'public')));
-  app.use('/assets/images', express.static(path.join(rootDir, 'public')));
-});
-
-app.configure('development', function(){
-
-  app.use('/app', express.static(path.join(rootDir, 'app')));
-  app.use('/.grunt', express.static(path.join(rootDir, '.grunt')));
-  app.use('/test/spec', express.static(path.join(rootDir, 'test', 'spec')));
-  app.use('/spec', function (req, res) {
-    res.sendfile(path.join(rootDir, '_SpecRunner.html'));
+  app.configure(function(){
+    app.set('port', process.env.PORT || 3000);
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.compress());
+    app.use('/assets', express.static(path.join(rootDir, 'public')));
+    app.use('/assets/images', express.static(path.join(rootDir, 'public')));
   });
-  app.use(express.errorHandler());
 
-  app.get('/stagecraft-stub/*', requirejs('../support/stagecraft_stub/stagecraft_stub_controller'));
+  app.configure('development', function(){
 
-  app.get('/backdrop-stub/*', requirejs('../support/stagecraft_stub/stagecraft_stub_controller'));
+    app.use('/app', express.static(path.join(rootDir, 'app')));
+    app.use('/.grunt', express.static(path.join(rootDir, '.grunt')));
+    app.use('/test/spec', express.static(path.join(rootDir, 'test', 'spec')));
+    app.use('/spec', function (req, res) {
+      res.sendfile(path.join(rootDir, '_SpecRunner.html'));
+    });
+    app.use(express.errorHandler());
+
+    app.get('/stagecraft-stub/*', requirejs('../support/stagecraft_stub/stagecraft_stub_controller'));
+
+    app.get('/backdrop-stub/*', requirejs('../support/stagecraft_stub/stagecraft_stub_controller'));
+  });
+
+
+  var render = requirejs('./render');
+  app.use('/performance', render);
+
+
+  app.get('/_status', requirejs('healthcheck_controller'));
+
+  var server = {
+    listen: http.createServer(app).listen(app.get('port'), function(){
+      console.log("Express server listening on port " + app.get('port'));
+    })
+  }
+
+  server.use = function() {
+    app.use.apply(app, arguments);
+  };
+
+  return server;
 });
-
-
-var render = requirejs('./render');
-app.use('/performance', render);
-
-
-app.get('/_status', requirejs('healthcheck_controller'));
-
-var server = http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
-
-
-exports = module.exports = server;
-
-exports.use = function() {
-	app.use.apply(app, arguments);
-};
