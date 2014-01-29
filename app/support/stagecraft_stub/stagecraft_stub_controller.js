d1ba33f3f877cbd20ee4cb476fedc680eedba752
Jacob Ashdown <jcbashdown@gmail.com>
2014-01-29 14:42:01 +0000

    Doesn't quite work but something like the way an mvp template could work

--- app/support/stagecraft_stub/stagecraft_stub_controller.js
+++ app/support/stagecraft_stub/stagecraft_stub_controller.js
@@ -1,15 +1,26 @@
 define(function () {
   var fs = requirejs('fs');
+  var Mustache = requirejs('vendor/mustache');
 
   return function (req, res) {
     var paramPath = req.params[0],
-        filePath = 'app/support/stagecraft_stub/responses/' + paramPath + '.json';
+        filePath = 'app/support/stagecraft_stub/responses/' + paramPath + '.json',
+        content;
     if (fs.existsSync(filePath)) {
-      var content = fs.readFileSync(filePath);
+      content = fs.readFileSync(filePath);
       res.send(JSON.parse(content));
     } else {
-      res.status(404);
-      res.send({error: "No such stub exists: " + filePath});
+      filePath = 'app/support/stagecraft_stub/configs/' + paramPath + '.json';
+      templatePath = 'app/support/stagecraft_stub/templates/mvp-service-dashboard.json';
+      if (fs.existsSync(filePath)) {
+        var config_json = JSON.parse(fs.readFileSync(filePath));
+        content = Mustache.render(fs.realpathSync(templatePath), config_json);
+        console.log(content);
+        res.send(JSON.parse(content.toString()));
+      }else{
+        res.status(404);
+        res.send({error: "No such stub exists: " + filePath});
+      }
     }
   };
 });
